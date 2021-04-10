265b8c462f6ee69576e3d88e93935d1493184210
longhorn2000 <80234800+longhorn2000@users.noreply.github.com>
2021-04-09 23:35:49 -0500

    PyPoll script

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,73 +1,66 @@
 #import poll
 import os
 import csv
-#working directory
+
 csvpath=os.path.join('..','Resources','election_data.csv')
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     print(csvreader)
     csv_header = next(csvreader)
+
+    #Declare variables as empty dictionaries and lists
+    Candidates = {}
+    Count = 0
+    Votes_Cast = 0
+    percent_of_votes = 0
+    Most_Votes = 0
+    Most_Voted = ""
     
-    #Declaring variables
-    votes = []
-    county = []
-    candidates = []
-    khan = []
-    correy = []
-    li = []
-    otooley = []
-
-
     for row in csvreader:
-        votes.append(int(row[0]))
-        county.append(row[1])
-        candidates.append(row[2])
-
-    #VOTE COUNT
-    total_votes = (len(votes))
-    print(total_votes)
-
-    #Votes by Person
-    for candidate in candidates:
-        if candidate == "Khan":
-            khan.append(candidates)
-            khan_votes = len(khan)
-        elif candidate == "Correy":
-            correy.append(candidates)
-            correy_votes = len(correy)
-        elif candidate == "Li":
-            li.append(candidates)
-            li_votes = len(li)
+        
+        # Count the total number of votes cast
+        candidate = row[2]
+        Count += 1
+        if candidate in Candidates.keys():
+            Candidates[candidate] += 1
         else:
-            otooley.append(candidates)
-            otooley_votes = len(otooley)
-    print(khan_votes)
-    print(correy_votes)
-    print(li_votes)
-    print(otooley_votes)
+            Candidates[candidate] = 1
+        #print(Candidates)
     
     
-    #Percentages
-    khan_percent = round(((khan_votes / total_votes) * 100), 2)
-    correy_percent = round(((correy_votes / total_votes) * 100), 2)
-    li_percent = round(((li_votes / total_votes) * 100), 2)
-    otooley_percent = round(((otooley_votes / total_votes) * 100), 2)
-    print(khan_percent)
-    print(correy_percent)
-    print(li_percent)
-    print(otooley_percent)
+    # Print Statements
+    print("Election Results")
+    print("-------------------------------")
+    print(f"Total Votes: {Count}")
+    print("-------------------------------")
     
-    #Winner 
-    if khan_percent > max(correy_percent, li_percent, otooley_percent):
-        winner = "Khan"
-    elif correy_percent > max(khan_percent, li_percent, otooley_percent):
-        winner = "Correy"  
-    elif li_percent > max(correy_percent, khan_percent, otooley_percent):
-        winner = "Li"
-    else:
-        winner = "O'Tooley"
-    
-
-
-
             
+    #total number of votes for each candidate
+    for candidate in Candidates:
+        Votes_Cast += Candidates[candidate]
+    
+        # percent of votes for each candidate
+        percent_of_votes = (Candidates[candidate])/(Count) * 100
+        print(f"{candidate}: {int(percent_of_votes)}% {Votes_Cast}")
+        
+        if Candidates[candidate] > Most_Votes:
+            Most_Voted = candidate
+            Most_Votes = Candidates[candidate]
+        
+        
+    # The winner of the election based on popular vote.
+    print("-------------------------------")
+    
+    print(f"Winner: {Most_Voted}")
+    
+    print("-------------------------------")
+   
+
+
+
+
+
+
+
+
+    
