eb80ba4775baeab73fabfda249a3da4d129e00b4
longhorn2000 <80234800+longhorn2000@users.noreply.github.com>
2021-04-09 19:33:34 -0500

    Test Save

---
+++ PyPoll/main.py
@@ -0,0 +1,18 @@
+#import poll
+import os
+import csv
+#working directory
+csvpath=os.path.join('..','Resources','election_data.csv')
+with open(csvpath, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    print(csvreader)
+    csv_header = next(csvreader)
+    
+    #Declaring variables
+    votes = []
+    county = []
+    candidates = []
+    khan = []
+    correy = []
+    li = []
+    otooley = []
